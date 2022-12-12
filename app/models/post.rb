4d01469d6995740672d8462aaff1bbe2b6dfb4ab
drm298git <hiroki.hayase.0608@gmail.com>
2022-12-12 14:18:39 +0900

    バリデーションを設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,8 +6,13 @@ class Post < ApplicationRecord
   validate :start_end_check#終了日が開始日より早く設定できないように設定
 
   def start_end_check
-    errors.add(:end_date, "は開始日より前の日付は登録できません。") unless
-    self.start_at < self.end_at 
-  end
+    if :start_at.nil? 
+      redirect_to :posts
+    elsif :start_at.nil? && :end_at.nil?
+        redirect_to :posts
+    else :start_at < :end_at
+      errors.add(:end_date, "は開始日より前の日付は登録できません。")
+    end
+  end 
 
 end
